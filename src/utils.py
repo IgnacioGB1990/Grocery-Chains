097c066426e99eae576b50f693ffb37f1989509d
IgnacioGB1990 <igarciabarrero@outlook.com>
2023-03-08 20:02:02 +0100

    Added some spiders

--- src/utils.py
+++ src/utils.py
@@ -1,46 +1,59 @@
 alimentacion = {
-    "Shushi":"https://www.alcampo.es/compra-online/frescos/sushi/c/W140502?q=%3Arelevance&page=0"
-    "Leche Huevos Yogures y Lácteos": "https://www.alcampo.es/compra-online/alimentacion/leche-huevos-yogures-y-lacteos/c/W16?q=%3Arelevance&page=0",
-    "Conservas": "https://www.alcampo.es/compra-online/alimentacion/conservas/c/W1004?q=%3Arelevance&page=0",
-    #  "Caldos, Pasta, Arroz Legumbres Puré": "https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Arelevance%3AcategoryChild%3AW140",
-    #  "Aceite Vinagre Salsas Especias": "https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Arelevance%3AcategoryChild%3AW18",
-    #  "Comida Internacional":"https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Arelevance%3AcategoryChild%3AW9410",
-    #  "Azúcar Harina Masas y Pan": "https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Arelevance%3AcategoryChild%3AW1009",
-    #  "Desayuno y Merienda": "https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Arelevance%3AcategoryChild%3AW10",
-    #  "Aperitivos, aceitunas y frutos secos":"https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Arelevance%3AcategoryChild%3AW120",
-    #  "Alimentos Especiales":"https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Arelevance%3AcategoryChild%3AW33",
-    #  "Supermercado Ecológico":"https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Arelevance%3AcategoryChild%3AW26112021",
-    #  "Nutrición deportiva":"https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Arelevance%3AcategoryChild%3AW12",
-    #  "Veganos":"https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Arelevance%3AcategoryChild%3AW09112021",
-    #  "Productos sin gluten": "https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Arelevance%3AcategoryChild%3AW8",
-    #  "Productos sin lactosa":"https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Arelevance%3AcategoryChild%3AW7"
+    "Leche Huevos Yogures y Lácteos": "https://www.alcampo.es/compra-online/alimentacion/leche-huevos-yogures-y-lacteos/c/W16?q=%3Arelevance&page=",
+    "Aceite Vinagre Salsas Especias" : "https://www.alcampo.es/compra-online/alimentacion/aceite-vinagre-salsas-especias/c/W18?q=%3Arelevance&page=",
+    "Desayuno y Merienda": "https://www.alcampo.es/compra-online/alimentacion/desayuno-y-merienda/c/W10?q=%3Arelevance&page=",
+    "Conservas": "https://www.alcampo.es/compra-online/alimentacion/conservas/c/W1004?q=%3Arelevance&page=",
+    "Comida Internacional":"https://www.alcampo.es/compra-online/alimentacion/comida-internacional/c/W9410?q=%3Arelevance&page=",
+    "Aperitivos, aceitunas y frutos secos": "https://www.alcampo.es/compra-online/alimentacion/aperitivos-aceitunas-y-frutos-secos/c/W120?q=%3Arelevance&page=",
+    "Nutrición deportiva":"https://www.alcampo.es/compra-online/alimentacion/nutricion-deportiva/c/W12?q=%3Arelevance&page=",
+    "Caldos, Pasta, Arroz Legumbres Puré":"https://www.alcampo.es/compra-online/alimentacion/caldos-pasta-arroz-legumbres-pure/c/W140?q=%3Arelevance&page=",
+    "Azúcar Harina Masas y Pan": "https://www.alcampo.es/compra-online/alimentacion/azucar-harina-masas-y-pan/c/W1009?q=%3Arelevance&page=",
+    "Alimentos Especiales": "https://www.alcampo.es/compra-online/alimentacion/alimentos-especiales-/c/W33?q=%3Arelevance&page=",
+    "Veganos": "https://www.alcampo.es/compra-online/alimentacion/veganos/c/W09112021?q=%3Arelevance&page=",
+    "Supermercado Ecológico": "https://www.alcampo.es/compra-online/alimentacion/supermercado-ecologico/c/W26112021?q=%3Arelevance&page="
 }
 
 frescos = {
-    "Plátanos y Bananas": "https://www.alcampo.es/compra-online/frescos/frutas/c/W1701?q=%3Arelevance%3AcategoryChild%3AW170104",
-    "Cítricos": "https://www.alcampo.es/compra-online/frescos/frutas/c/W1701?q=%3Arelevance%3AcategoryChild%3AW170101",
-    "Tropicales y Exóticas":"https://www.alcampo.es/compra-online/frescos/frutas/c/W1701?q=%3Arelevance%3AcategoryChild%3AW170106",
-    "Fresón y frutas del bosque":"https://www.alcampo.es/compra-online/frescos/frutas/c/W1701?q=%3Arelevance%3AcategoryChild%3AW170109",
-    "Frutas de temporada":"https://www.alcampo.es/compra-online/frescos/frutas/c/W1701?q=%3Arelevance%3AcategoryChild%3AW170105",
-    "Peras y Manzanas":"https://www.alcampo.es/compra-online/frescos/frutas/c/W1701?q=%3Arelevance%3AcategoryChild%3AW170102001",
-    "Sandías y Melones":"https://www.alcampo.es/compra-online/frescos/frutas/c/W1701?q=%3Arelevance%3AcategoryChild%3AW170110",
-    "Fruta cortada":"https://www.alcampo.es/compra-online/frescos/frutas/c/W1701?q=%3Arelevance%3AcategoryChild%3AW170105003",
-    "Frutas ecológicas":"https://www.alcampo.es/compra-online/frescos/frutas/c/W1701?q=%3Arelevance%3AcategoryChild%3AW170107",
-    "Puré y snacks de frutas": "https://www.alcampo.es/compra-online/frescos/frutas/c/W1701?q=%3Arelevance%3AcategoryChild%3AWPureSnacksFrutas",
-    "Zumo exprimido": "https://www.alcampo.es/compra-online/frescos/frutas/c/W1701?q=%3Arelevance%3AcategoryChild%3AW12082022",
-    "Fruta Premium": "https://www.alcampo.es/compra-online/frescos/frutas/c/W1701?q=%3Arelevance%3AcategoryChild%3AW101020221"
+    "Frutas": "https://www.alcampo.es/compra-online/frescos/frutas/c/W1701?q=%3Arelevance&page=",
+    "Carne": "https://www.alcampo.es/compra-online/frescos/carne/c/W13?q=%3Arelevance&page=",
+    "Panadería":"https://www.alcampo.es/compra-online/frescos/panaderia/c/W1281?q=%3Arelevance&page=",
+    "Verduras y hortalizas":"https://www.alcampo.es/compra-online/frescos/verduras-y-hortalizas/c/W1702?q=%3Arelevance&page=",
+    "Charcutería":"https://www.alcampo.es/compra-online/frescos/charcuteria/c/W15?q=%3Arelevance&page=",
+    "Pastelería":"https://www.alcampo.es/compra-online/frescos/pasteleria/c/W1282?q=%3Arelevance&page=",
+    "Sushi": "https://www.alcampo.es/compra-online/frescos/sushi/c/W140502?q=%3Arelevance&page=",
+    "Quesos": "https://www.alcampo.es/compra-online/frescos/quesos/c/WQuesos?q=%3Arelevance&page=",
+    "Pescados, mariscos y moluscos": "https://www.alcampo.es/compra-online/frescos/pescados-mariscos-y-moluscos/c/W14?q=%3Arelevance&page=",
+    "Ahumados, sucedáneos gulas, anchoas": "https://www.alcampo.es/compra-online/frescos/ahumados-sucedaneos-gulas-anchoas/c/W184?q=%3Arelevance&page=",
+    "Fresco al Corte": "https://www.alcampo.es/compra-online/frescos/fresco-al-corte/c/WFrescoalcorte?q=%3Arelevance&page="
+}
+
+bebidas = {
+    "Bebidas refrescantes": "https://www.alcampo.es/compra-online/bebidas/bebidas-refrescantes/c/W1103?q=%3Arelevance&page=",
+    "Cervezas":"https://www.alcampo.es/compra-online/bebidas/cervezas/c/W1107?q=%3Arelevance&page=",
+    "Vino Tinto":"https://www.alcampo.es/compra-online/bebidas/vino-tinto/c/W1151?q=%3Arelevance&page=",
+    "Zumos de Frutas":"https://www.alcampo.es/compra-online/bebidas/zumos-de-frutas/c/W1102?q=%3Arelevance&page=",
+    "Bebidas Alcohólicas":"https://www.alcampo.es/compra-online/bebidas/bebidas-alcoholicas/c/W1154?q=%3Arelevance&page=",
+    "Vino Blanco": "https://www.alcampo.es/compra-online/bebidas/vino-blanco/c/W1152?q=%3Arelevance&page=",
+    "Agua, Soda y Gaseosas": "https://www.alcampo.es/compra-online/bebidas/agua-soda-y-gaseosas/c/W1101?q=%3Arelevance&page=",
+    "Licores": "https://www.alcampo.es/compra-online/bebidas/licores/c/W1155?q=%3Arelevance&page=",
+    "Vino rosados, frizzantes, dulces y olorosos":"https://www.alcampo.es/compra-online/bebidas/vino-rosados-frizzantes-dulces-y-olorosos/c/W1153?q=%3Arelevance&page=",
+    "Champagne Cavas y Sidras": "https://www.alcampo.es/compra-online/bebidas/champagne-cavas-y-sidras/c/W1156?q=%3Arelevance&page=",
+}
+
+drogueria = {
+    "Lavado de Ropa": "https://www.alcampo.es/compra-online/drogueria/lavado-de-ropa/c/W2104?q=%3Arelevance&page=",
+    "Lavavajillas": "https://www.alcampo.es/compra-online/drogueria/lavavajillas/c/W2106?q=%3Arelevance&page=",
+    "Utensilios Limpieza": "https://www.alcampo.es/compra-online/drogueria/utensilios-limpieza/c/W2109?q=%3Arelevance&page=",
+    "Celulosas": "https://www.alcampo.es/compra-online/drogueria/celulosas/c/W2101?q=%3Arelevance&page=",
+    "Lejías y Amoníacos": "https://www.alcampo.es/compra-online/drogueria/lejias-y-amoniacos/c/W2107?q=%3Arelevance&page=",
+    "Limpieza Calzado": "https://www.alcampo.es/compra-online/drogueria/limpieza-calzado/c/W2105?q=%3Arelevance&page=",
+    "Conservación de Alimentos y Moldes": "https://www.alcampo.es/compra-online/drogueria/conservacion-de-alimentos-y-moldes/c/W2102?q=%3Arelevance&page=",
+    "Limpieza Hogar": "https://www.alcampo.es/compra-online/drogueria/limpieza-hogar/c/W2108?q=%3Arelevance&page=",
+    "Ambientadores": "https://www.alcampo.es/compra-online/drogueria/ambientadores/c/W2110?q=%3Arelevance&page=",
+    "Insecticidas" : "https://www.alcampo.es/compra-online/drogueria/insecticidas/c/W2111?q=%3Arelevance&page="
+
 }
 
 generica = "https://www.alcampo.es/compra-online/alimentacion/c/WC10"
-
-
-import pygame
-from pygame import mixer
-
-def playSound():
-    mixer.init()
-    mixer.music.load("././assets/completed.mp3")
-    mixer.music.set_volume(0.4)
-    mixer.music.play()
 
 
